420c902011e82507cf5318f17a0646a30197f999
Vinod Kumar <vinodrocky504@gmail.com>
2019-03-13 08:08:57 +0530

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,8 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-  //  const port = 8000 // Change this to your server port
-    return `../js/restaurants.json`;
+  const port = 8000 // Change this to your server port
+    return './js/restaurants.json';
   }
 
   /**
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/js/${restaurant.photograph}`);
+    return (`./js/${restaurant.photograph}`);
   }
 
   /**
