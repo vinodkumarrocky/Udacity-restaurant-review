628cb160f5cef7cd027a2fce4b1946e6d4eabb97
vinod kumar <vinodrocky504@gmail.com>
2019-03-13 07:42:33 +0530

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
   //  const port = 8000 // Change this to your server port
-    return `/Udacity-restaurant-review/js/restaurants.json`;
+    return `https://www.filehosting.org/file/details/787072/restaurants.json`;
   }
 
   /**
